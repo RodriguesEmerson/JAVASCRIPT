0d3edb2b21eaa3a9dd2f11f86fe4c3a2400efd4f
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-02-09 14:45:56 -0300

    Jogo da Velha
    Refaturei o código

---
+++ desafios/jogo-da-velha/main.js
@@ -0,0 +1,48 @@
+let square = document.querySelectorAll('.square')
+let btn = document.querySelector('.btn-play')
+let namePlayer = document.querySelectorAll('.player-Name')
+let start = document.querySelector('.start')
+
+btn.addEventListener('click', () => {
+    start.style.visibility = 'hidden'
+    for(i in square){
+        square[i].innerText = ''
+    }
+})
+
+for(let ind = 0; ind < square.length; ind++){
+    square[ind].addEventListener('click', () => {
+        jogo.play(square[ind], jogo.character)
+        jogo.checkWinner()
+    })
+}
+let jogo = {
+    players: [namePlayer[0],[namePlayer[1]]],
+    player: 1,
+    character: 'X',
+    play: function(s ,v){
+        if(s.innerText == '') s.innerText = v;
+        this.player = (this.player == 1 ? 0 : 1)
+        this.character = (this.character == 'X' ? 'O' : 'X')
+    },
+    win:[
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6],
+    ],
+    checkWinner: function(v){
+        v = (this.player == 0 ? 'X' : 'O')
+        for( i in this.win){
+            if(square[this.win[i][0]].innerText == v && 
+            square[this.win[i][1]].innerText == v && 
+            square[this.win[i][2]].innerText == v){
+                this.Winner()
+            }
+        }
+    },
+    Winner: function(){
+       start.style.visibility = 'visible'
+        this.character = 'X'
+        this.player = 1;
+    }
+}
--- desafios/jogo-da-velha/script.js
+++ desafios/jogo-da-velha/script.js
@@ -1,28 +1,15 @@
 let square = document.querySelectorAll('.square')
 
-
 for(let ind = 0; ind < square.length; ind++){
     square[ind].addEventListener('click', () => {
         addPlay(square[ind])
     })
 }
-
-//let play = 'User'
 function addPlay(which){
-    
-   //if(play == 'User'){
     which.innerText = 'V'
-    //play = 'Machine'
     Winner('V')
     MachinePlay()
-   //} else{
-    //which.innerText = 'O'
-    //play = 'User'
-    //Winner('O')
-   //}   
-   
 }
-
 function Winner(v){
     if(square[0].innerText == v && square[1].innerText == v && square[2].innerText == v) return console.log('winner')
     if(square[3].innerText == v && square[4].innerText == v && square[5].innerText == v) return console.log('winner')
@@ -39,7 +26,6 @@ function Winner(v){
 
 function MachinePlay(){
     let teste = false
-    
 
     for(let ind = 1; ind < square.length; ind++){
         if(square[ind].innerText == '')  teste = true
