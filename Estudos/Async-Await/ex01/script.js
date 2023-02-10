49187133e7adbada7f10c0a53c5a78cf5742060c
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-02-10 17:55:13 -0300

    Progredindo nos estudos!
    Finalmente entendi a lógica do async e await e do uso de APIs.

---
+++ Estudos/Async-Await/ex01/script.js
@@ -0,0 +1,37 @@
+//Sintaxe
+// function PriFunc(id){
+//     return fetch(`https://reqres.in/api/users?id=${id}`)
+//     .then((data) => data.json())
+//     .catch((err) => console.log(err))
+// }
+
+// async function getUser(id){
+//     const user = await PriFunc(id)
+    
+//     console.log(user)
+//     console.log(user.data)
+//     console.log(user.data.first_name)
+    
+// }
+// getUser(12)
+
+//Sintaxe
+function PriFunc(id){
+    return fetch(`https://reqres.in/api/users?id=${id}`)
+    .then((data) => data.json())
+    .catch((err) => console.log(err))// trata erro da promise
+}
+
+async function getUser(id){
+    try{
+        const user = await PriFunc(id)
+    
+        console.log(user)
+        console.log(user.data)
+        console.log(user.data.first_name)
+    }catch(err){ // trata erro do async
+        console.log(`Erro: ${err}`)
+    }
+    
+}
+getUser(12)
