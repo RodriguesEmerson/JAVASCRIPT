c4f12789e2b2590ba3757dac368165ddd154513d
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-02-11 10:38:47 -0300

    Update script.js

--- Estudos/Async-Await/ex01/script.js
+++ Estudos/Async-Await/ex01/script.js
@@ -18,14 +18,14 @@
 //Sintaxe
 function PriFunc(id){
     return fetch(`https://reqres.in/api/users?id=${id}`)
-    .then((data) => data.json())
+    .then((data) => data.json()) //Transforma os dados em objetos para o JS
     .catch((err) => console.log(err))// trata erro da promise
 }
 
 async function getUser(id){
     try{
         const user = await PriFunc(id)
-    
+       
         console.log(user)
         console.log(user.data)
         console.log(user.data.first_name)
