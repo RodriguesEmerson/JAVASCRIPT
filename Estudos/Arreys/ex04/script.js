0ab42f310de724fb21963da944bab443b2794803
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-02-18 09:05:56 -0300

    Estudando

--- Estudos/Arreys/ex04/script.js
+++ Estudos/Arreys/ex04/script.js
@@ -1,18 +1,26 @@
-function hangman(hangmanString) {
-    let e = 1
-    hangmanString = hangmanString.split('')
-    for(let i = 0 ; i < hangmanString.length; i++){
-        if(e == 1){
-            console.log('foi')
-            hangmanString[i] = '_'
-            e = 2
-        }else{
-            e = 1
-        }
-    }
-    hangmanString = hangmanString.toString()
-    hangmanString = hangmanString.replace(/\,/g, '')
-    console.log(hangmanString)
+function checkPassword(password, password_repeat) {
+    let regMai = /[A-Z]/
+    let regMin = /[a-z]/
+    let hasNum = /\d/
+    let espChars = /\&|\$|\%|\§|\-|\_/
+    let quant = ''
+    let equal = ''
+
+    if(password.length >= 8) quant = true
+    if(password == password_repeat) equal = true
+    
+    if(regMai.test(password) == true &&
+    regMin.test(password == true) &&
+    hasNum.test(password) == true &&
+    espChars.test(password) == true &&
+    quant == true && equal == true){
+        console.log('true')
+    }else(
+        console.log('false')
+    )
     
 }
-hangman('TestString')
+
+checkPassword('omvdsse', 'omvdsse')
+checkPassword('HUIFDSJHKSADN', 'HUIFDSJHKSADN')
+checkPassword('YY&glk4Hfi_ffS', 'YY&glk4Hfi_ffS')
