ddc4691e5356c72f2f505f5e336ffd2fd708fd14
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-03-08 15:38:29 -0300

    Gráfico de Barras
    Finalizei mais este desafio, e foi muito mais difícil que o gráfico de linhas.

--- desafios/graficos/grafico-de-barras/js/script.js
+++ desafios/graficos/grafico-de-barras/js/script.js
@@ -3,13 +3,14 @@ let container = document.querySelector('.cont-canvas')
 
 let [labelSize, labelText, newLabel, labelHalf,
      barWidth, barWidthRaw, barHeight, barTop, barLeft, gap,
-     canvasWidth, canvasHeight, maxValue, divisor
+     canvasWidth, canvasHeight, maxValue, divisor,
+     index
     ] = [];
 
 //305, 435, 623, 730, 123, 321, 238, 234
-let dados = [300, 231, 305, 435, 730, 321]
-let colors = ['blueviolet', 'yellow', 'blue', 'green', 'brown', 'red']
-let labels = ["Label1", "Label2"]
+let dados = [300, 231, 305, 435, 730]
+let colors = ['blueviolet', 'chocolate', 'blue', 'green', 'brown', 'red']
+let labels = ["Label1", "Label2", "Label3", "Label5", "Label5", "Label6"]
 let arrLength = dados.length;
 let draw = canvas.getContext("2d");
 
@@ -17,30 +18,35 @@ canvasWidth = canvas.offsetWidth;
 canvasHeight = canvas.offsetHeight;
 barWidthRaw = canvasWidth / arrLength;
 
-barLeft = (barWidthRaw / 2);
+barLeft = barWidthRaw / 2;
 
-barWidth = barWidthRaw - (barLeft * arrLength ); /**Width de cada barra - barLeft * arrLength */
-gap = barWidth + barWidth / 2;  /**Distância entre uma barra e outra */
+
+
+barWidth = barLeft + (barLeft / 2); /**Width de cada barra*/
+gap = barLeft * 1.9;  /**Distância entre uma barra e outra */
+
+barLeft = barLeft / 2;
 
 maxValue = Math.max(...dados);
 divisor = (1,11/1000) * maxValue;
-
 
 /**Funão auto-executável */
 ;(function drawBarras(){
 
     dados.forEach(element => {
+        index = dados.indexOf(element)
         barHeight = (element/100) * canvasHeight / divisor; /**Altura da barra | valor / 100 * canvasHeigth / divisor */
         barTop = canvasHeight - barHeight;
-        
         /**Desenha a Barra */
-        draw.fillStyle = colors[dados.indexOf(element)]
-        draw.fillRect(Math.abs(barLeft), barTop, barWidth, barHeight); /**left, top, width, height */
+        draw.fillStyle = colors[index]
+        draw.fillRect(barLeft, barTop, barWidth, barHeight); /**left, top, width, height */
 
-        labelSize = 15
-        labelText = 'Label'
+        
 
         /**Criando a Label de cada barra */
+        labelSize = 15
+        labelText = labels[index]
+
         newLabel = document.createElement('span')
             newLabel.textContent = labelText
             newLabel.style.fontSize = `${labelSize}px`
@@ -60,6 +66,7 @@ divisor = (1,11/1000) * maxValue;
 
 })();
 
+//valor base * (porcentagem/100).
 /** ======================== LÓGICA =============================
  * 
  * 1 - Calcular a width de cada barra com base na quantidade
@@ -74,9 +81,7 @@ divisor = (1,11/1000) * maxValue;
  *      | (% / 1000) * maiorValor
  * 
  * 3 - O gap entre cada barra deverá ser calculado com base
- * na quantidade de barras, ou seja, em porcetagem. Por 
- * enquanto fica 50% multiplicado pela quantidade de itens 
- * no array como base de cálculo.
+ * na quantidade de barras, ou seja, em porcetagem. 
  * 
  * 4 - o Texto deve ficar 5px acima de cada barra, sendo assim,
  * basta pegar a altura da barra subtrair 5px. Já o left do texto,
@@ -87,14 +92,9 @@ divisor = (1,11/1000) * maxValue;
  * na label do grafico. Pegar a width da label, a dividir por 2 e tendo
  * essa medida, diminuir da distancia left da barra mais a metade da
  * width da barra desse valor que conseguir.
- *      | leftBarra + (widthBarra / 2) - (widthLabel / 2), (TopBarra - 5)
+ *      | leftBarra + (widthBarra / 2) - (widthLabel / 2), (TopBarra - 5);
+ * 
+ * OBS: Alguns dos números tive que procurar o que dava o melhor
+ * resultado, como o 1.9 do 'gap'.
  */
-
-
-// console.log(Math.max(...dados))
-
-/**Desenha a borda do Gráfico 
-*   draw.strokStyle = 'Black';
-*   draw.strokeRect(30, 120, 30, 180)
-*/
 
