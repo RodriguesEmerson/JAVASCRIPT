5b5c0071985861532832aec3e19b4f4a7422d530
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-03-17 17:28:30 -0300

    Concluindo Drag and Drop

--- projetos/clone-trello/js/modules/drag-drop.js
+++ projetos/clone-trello/js/modules/drag-drop.js
@@ -1,98 +1,33 @@
-let listItem, column, tempBack, tempDiv, tempContet;
-(function selector() {
-    listItem = document.querySelectorAll('.list-content');
-    column = document.querySelectorAll('.drag-area');
-})();
-const board = document.querySelector('.board')
 
-tempBack = document.createElement('div')
-tempBack.setAttribute('class', 'tempBack')
-
-tempDiv = document.createElement('div')
-tempDiv.setAttribute('class', 'tempDiv')
-
-
-
-let positionMouseClicked_X, positionMouseClicked_Y;
-listItem[0].addEventListener('dragstart', (element) => {
-    
-    listItem[0].classList.add('dragging')
-
-    let elementPosition = listItem[0].getBoundingClientRect();
-    positionMouseClicked_X = Math.abs(elementPosition.left - element.clientX)
-    positionMouseClicked_Y = Math.abs(elementPosition.top - element.clientY)
-    
-})
-listItem[0].addEventListener('dragover', (element) => {
-
-    let itemHeight = listItem[0].offsetHeight;
-    column[0].appendChild(tempBack)
-
-    tempDiv.style.height = `${itemHeight}px`
-    tempDiv.innerHTML = listItem[0].innerHTML
-   // board.appendChild(tempDiv)
-    
-   
-    listItem[0].style.position = 'absolute'
-    listItem[0].style.left = (element.clientX - positionMouseClicked_X) + 'px'
-    listItem[0].style.top = (element.clientY - positionMouseClicked_Y) + 'px'
-
-    
-    //console.log('Left: ' + (element.clientX - positionMouseClicked_X) + 'px')
-    // console.log('Position Atual: ' + element.clientX)
-
-})
-
-listItem[0].addEventListener('dragend', (element) => {
-
-    let dragging = document.querySelector('.dragging');
-    dragging.style = ''
-
-    
-    column[0].removeChild(tempBack)
-
-    column[0].prepend(dragging)
-    dragging.classList.remove('dragging')
-
-    
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**=====NÃO GOSTEI DESSE RESULTADO===== */
-/*
-let listItem, column;
+let listItem, column, tempDiv;
 (function selector(){
     listItem = document.querySelectorAll('.list-content');
     column = document.querySelectorAll('.drag-area');
 })();
 
+
+let elementMoving;
 for ( let i = 0; i < listItem.length; i++ ){
+
+    /**=============DragStart================ */
     listItem[i].addEventListener('dragstart', () => {
+
+        tempDiv = document.createElement('div')
+        tempDiv.setAttribute('class', 'tempDiv')
+
         listItem[i].classList.add('dragging')
     })
 
+    /**=============DragStart================ */
     listItem[i].addEventListener('dragend', () => {
+
         listItem[i].classList.remove('dragging')
+        listItem[i].style.display = 'flex'
+
+        let tempDIV = document.querySelector('.tempDiv')
+        tempDIV.parentNode.replaceChild(elementMoving, tempDIV) //substitui a div temporaria 'sombra'.
+            
+        
     })
 }
 
@@ -100,14 +35,17 @@ column.forEach( item => {
     item.addEventListener('dragover', ( element => {
         
         let dragging = document.querySelector('.dragging');
+        dragging.style.display = 'none';
+
+        elementMoving = dragging;
+
         let applayAfter = getNewPosition(item, element.clientY);
        
         if( applayAfter ){
-            applayAfter.insertAdjacentElement('afterend', dragging);
+            applayAfter.insertAdjacentElement('afterend', tempDiv);
         }else{
-            item.prepend(dragging);
+            item.prepend(tempDiv);
         }
-        
     }));
 
    
@@ -127,4 +65,3 @@ function getNewPosition( column, positionY ){
     }
     return result;
 }
-*/
