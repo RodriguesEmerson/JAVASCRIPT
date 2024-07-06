be3e5ce97b7be9f83ba1983afe9c7437c3dfb464
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-07-06 17:38:59 -0300

    Update script.js

--- desafios/Digitacao/script.js
+++ desafios/Digitacao/script.js
@@ -4,22 +4,23 @@ let textoDigitado = document.getElementById('text-input')
 let base = 'd';
 
 btnStart.addEventListener('click', test)
-textoDigitado.addEventListener('keydown', verificaMatch)
+textoDigitado.addEventListener('keydown', verificaMatch())
 
-function test(){
+function test() {
     let caracterAtual = textBase.textContent.charAt(23)
-    if(base === caracterAtual){
+    if (base === caracterAtual) {
         console.log('Igual')
-    }else{
+    } else {
         console.log('Atual')
     }
 }
 
-function verificaMatch(){
-    let caracterAtualNumber = Number(textoDigitado.value.length)
-    let caracterAtual = textoDigitado.value.charAt(caracterAtualNumber -1)
+function verificaMatch() {
+    let caracterAtualNumber = Number(textoDigitado.value.length - 1)
+    let caracterAtual = textoDigitado.value.charAt(caracterAtualNumber)
     let caracterAtualBase = textBase.textContent.charAt(caracterAtualNumber + 1)
+    let test= KeyboardEvent.length
+    console.log(test)
 
-
-    console.log(`Numero: ${caracterAtualNumber} Letra: ${caracterAtual} Base:${caracterAtualBase}`)
+    console.log(`Index: ${caracterAtualNumber} Letra: ${caracterAtual} Base:${caracterAtualBase}`)
 }
