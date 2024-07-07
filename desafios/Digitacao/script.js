b6bbe141d290fe0a2afdd00047cc9783c9bcebd8
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-07-07 17:47:11 -0300

    Aprimorando Teste de Digitacao

--- desafios/Digitacao/script.js
+++ desafios/Digitacao/script.js
@@ -1,15 +1,13 @@
 let textBase = document.querySelector('.text-model p');
 let btnStart = document.querySelector('#btn-start-again')
 let textoDigitado = document.getElementById('text-input')
-let teclaErro
-let destacarErro
-
+let errou = false;
 
 let caracteresValidos = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'x', 'y', 'w', 'z', 'ç', ',', '.', '!',
     '?', ':', 'ã', 'á', 'à', 'â', 'é', 'ê', 'í', 'ú',
-    'õ', 'ó', 'ô', ' '
+    'õ', 'ó', 'ô', ' ',
 ]
 
 textoDigitado.addEventListener('keydown', keyValida)
@@ -17,43 +15,41 @@ textoDigitado.addEventListener('keydown', keyValida)
 function keyValida(e) {
     for (let ind = 0; ind < caracteresValidos.length; ind++) {
         if (e.key == caracteresValidos[ind]) {
+
             //console.log(caracteresValidos[ind])
             verificaMatch(e.key)
+            if (errou) {
+                e.preventDefault()
+            }
             return
         }
         if (e.key == caracteresValidos[ind].toUpperCase()) {
             //console.log(e.key)
             verificaMatch(e.key)
+            if (errou) {
+                e.preventDefault()
+            }
             return
         }
     }
+
 }
 
 function verificaMatch(e) {
     console.log('*****************')
-    let caracterAtualIndex = Number(textoDigitado.value.length + 1)
+    let caracterAtualIndex = Number(textoDigitado.value.length)
     let caracterAtual = textBase.textContent.charAt(caracterAtualIndex)
 
     console.log('Base:' + caracterAtual)
     console.log('Tecla Pressionada:' + e)
 
     if (e == caracterAtual) {
-        teclaErro = false;
-        textoDigitado.style.color='black';
+        textoDigitado.style.color = 'black';
         console.log('Match:' + caracterAtual);
-    }else{
-        teclaErro = true;
-        textoDigitado.style.color='red';
+        errou = false;
+    } else {
+        textoDigitado.style.color = 'red';
+        errou = true;
     }
 
-    if(teclaErro){
-        let arrayErro = textBase.textContent.split('')
-        arrayErro[caracterAtualIndex] = `<span class = 'errou-essa-tecla'>${caracterAtual}</span>`;
-        console.log(arrayErro)
-      
-        textBase.innerHTML = arrayErro.toString();
-        console.log(textBase.innerHTML)
-        //destacarErro = document.querySelectorAll('.errou-essa-tecla');
-        //destacarErro.style.color= 'red';
-    }
 }
