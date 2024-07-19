5682fcb2f663ec64446a6c07eeff0ce656697ef6
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-07-19 14:44:58 -0300

    FInancas
    Concluído as funções para adicionar novos dados pata as tabelas.

--- desafios/Financas/script/lancar.js
+++ desafios/Financas/script/lancar.js
@@ -1,19 +1,31 @@
 import { criar } from './script.js';
-import { todosOsDados ,categorias } from './modules/dados.js';
+import { todosOsDados, categorias } from './modules/dados.js';
 const form = document.querySelector('.dados-box');
 const selectCategorias = document.querySelector('#lancar-categoria');
 const formBox = document.querySelector('.lancar-container');
 const abirForm = document.querySelector('#abir-lancar');
 const fecharForm = document.querySelector('#fechar-form');
-let id = 10;
 
 abirForm.addEventListener('click', () => {
     formBox.classList.remove('hidden')
     carregaCategorias()
 })
+
 fecharForm.addEventListener('click', () => {
     formBox.classList.add('hidden');
 })
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const dados = Object.fromEntries(formData);
+    const novoLancamento = novoDado(dados)
+
+    todosOsDados[dados.tipo][2024]['JAN'].push(novoLancamento);
+    console.log(todosOsDados[dados.tipo][2024]['JAN']);
+});
+
 function carregaCategorias() {
     categorias[0].forEach(element => {
         const option = criar('option');
@@ -22,23 +34,23 @@ function carregaCategorias() {
         selectCategorias.appendChild(option);
     })
 }
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const dados = Object.fromEntries(formData);
-    const novoLancamento = novoDado(dados, id)
-    todosOsDados[dados.tipo][2024]['JAN'].push(novoLancamento)
-    console.log(novoLancamento)
-    console.log(todosOsDados[dados.tipo][2024]['JAN'])
-    
-});
 
-function novoDado (dados, id){
-    // let descricao = dados.descricao
-    // let data = dados.data;
-    // let categoria = dados.categoria;
-    // let valor = dados.valor
-    // let idd = id;
-    // const obj = {descricao, data, categoria, valor, idd}
+function novoDado(dados) {
+    let ordem = ['desc', 'data', 'categoria', 'valor'];
+    if (dados.tipo != 'despesas') ordem = ['desc', 'data', 'valor'];
+    let obj = {}
+    ordem.forEach(element => {
+        obj[element] = dados[element]
+    })
+    obj.id = radomID(2024, 'JAN');
+    return obj
+}
+
+function radomID(ano, mes) {
+    let min = Math.ceil(10);
+    let max = Math.floor(1000);
+    let id = Math.floor(Math.random() * (max - min) + min);
+    let newId = `${ano}${mes}${id}`
+    return newId;
 }
 
