b730f53af4defdacc3293ff19d97967a86bf5969
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-07-23 13:11:22 -0300

    Financas
    Configurando NavBar

--- desafios/Financas/script/modules/navigation.js
+++ desafios/Financas/script/modules/navigation.js
@@ -1,39 +1,59 @@
 const checboxAno = document.querySelectorAll('input[type=checkbox]');
 const liMeses = document.querySelectorAll('.link-mes');
-let anoClicado = 1;
-let mesClicado;
+let liAnteriorClicada;
 
 const MudarMesTabela = {
+    ulDoAnoClicado: '',
+    liDoMesClicado:'',
     anoClicado: '',
     mesClicado: '',
-    selecionaAnoMesClicado: function(){
-        this.mesClicado = this.getAttribute('value');
-        this.anoClicado = this.closest('ul')
+    selecionaAnoMesClicado: function(li){
+        this.liDoMesClicado = li;
+        this.ulDoAnoClicado = li.closest('ul');
+        this.mesClicado = li.getAttribute('value');
+        this.anoClicado = li.closest('ul')
             .getAttribute('id')
             .substring(1);
-     },
+        this.mudaCores();
+        },
+        mudaCores: function(){
+            this.liDoMesClicado.style.color = 'white';
+        },
      
 }
+
 function openCloseYearNav(){
     const ulPai = this.closest('ul');
     const ulPaiID = this.closest('ul').getAttribute('id');
     const sinalAbrirFechar = document.querySelector(`#${ulPaiID} .sinal-abir-fechar`);
-    
+
     if(this.checked){
-        sinalAbrirFechar.textContent = '+';
+        sinalAbrirFechar.textContent = '-';
         ulPai.style.height = 'auto';
     }else{
-        sinalAbrirFechar.textContent = '-';
+        sinalAbrirFechar.textContent = '+';
         ulPai.style.height = '20px';
     }
-};
+    
+    //Mudar cor do ano clicado;
+    const liDoAnoClicado = document.querySelector(`#${ulPaiID} .link-ano`);
+    if(sinalAbrirFechar.textContent == '-'){
+        liDoAnoClicado.style.color = 'white';
+    }
+    if(liAnteriorClicada){
+        liAnteriorClicada.style.color = 'gray';
+    }
+    liAnteriorClicada = liDoAnoClicado;
+    
+    
 
+};
 
 (function(){
     for(let ind = 0; ind < checboxAno.length; ind++){
         checboxAno[ind].addEventListener('change', openCloseYearNav);
     }
     for(let ind = 0; ind < liMeses.length; ind++){
-        liMeses[ind].addEventListener('click', MudarMesTabela.selecionaAnoMesClicado)
+        liMeses[ind].addEventListener('click', MudarMesTabela.selecionaAnoMesClicado.bind(MudarMesTabela, liMeses[ind]))
     }
 }());
