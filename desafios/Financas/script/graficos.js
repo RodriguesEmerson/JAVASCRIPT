2f4984b28dde4fc93c6caf1b8c2809db466ec653
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2024-08-01 14:52:49 -0300

    Financas

--- desafios/Financas/script/graficos.js
+++ desafios/Financas/script/graficos.js
@@ -1,10 +1,11 @@
 const graficoDespesas = document.querySelector('.grafico-despesas');
 const graficoReceitas = document.querySelector('.grafico-receitas');
+const graficoResumo = document.querySelector('.grafico-resumo');
 const legend = document.querySelector('#legend')
 let cor1 = 'rgb(200, 0, 0)';
 let llabel = ['Alimentação', 'Saúde', 'Casa', 'Pessoal']
 
-let ctx = new Chart(graficoDespesas, {
+let ctxGraficoDespesas = new Chart(graficoDespesas, {
     type: 'bar',
     data: {
         // labels: ['JAN', 'FEV', 'MAR']
@@ -38,7 +39,7 @@ let ctx = new Chart(graficoDespesas, {
         ],
     }
 });
-new Chart(graficoReceitas, {
+let ctxGraficoReceitas = new Chart(graficoReceitas, {
     type: 'doughnut',
     data: {
         // labels: ['JAN', 'FEV', 'MAR']
@@ -51,6 +52,41 @@ new Chart(graficoReceitas, {
 
     },
     options: {
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                fontSize: 15,
+                text: 'Gráfico Despesas'
+            },
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        backgroundColor: [
+            cor1,
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+    }
+})
+
+let ctxGraficoResumo = new Chart(graficoResumo, {
+    type: 'doughnut',
+    data: {
+        // labels: ['JAN', 'FEV', 'MAR']
+        labels: llabel,
+        datasets: [{
+            label: 'Gasto',
+            data: ['134', '492', '432'],
+            borderWidth: 1,
+        }]
+
+    },
+    options: {
+        maintainAspectRatio: true,
         plugins: {
             title: {
                 display: true,
